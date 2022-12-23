16bbdbbf625a354cd17d748189245c0010e9543a
Thi Dinh <vietthidinh2001@gmail.com>
2022-12-23 23:23:08 +0200

    add: make cpu be able to handle multiple commands

--- src/main.rs
+++ src/main.rs
@@ -15,19 +15,21 @@ impl CPU {
     }
 
     fn run(&mut self) {
-        // loop {
-        let opcode = self.read_opcode();
+        loop {
+            let opcode = self.read_opcode();
+            self.program_counter += 2;
 
-        let c = ((opcode & 0xF000) >> 12) as u8;
-        let x = ((opcode & 0x0F00) >> 8) as u8;
-        let y = ((opcode & 0x00F0) >> 4) as u8;
-        let d = ((opcode & 0x000F) >> 0) as u8;
+            let c = ((opcode & 0xF000) >> 12) as u8;
+            let x = ((opcode & 0x0F00) >> 8) as u8;
+            let y = ((opcode & 0x00F0) >> 4) as u8;
+            let d = ((opcode & 0x000F) >> 0) as u8;
 
-        match (c, x, y, d) {
-            (0x8, _, _, 0x4) => self.add_xy(x, y),
-            _ => todo!("opcode {opcode:04x}"),
-        };
-        // }
+            match (c, x, y, d) {
+                (0, 0, 0, 0) => return,
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
+                _ => todo!("opcode {:04x}", opcode),
+            };
+        }
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
@@ -53,4 +55,21 @@ fn main() {
         program_counter: 0,
         memory: [0; 0x1000],
     };
+
+    cpu.registers[0] = 5;
+    cpu.registers[1] = 10;
+    cpu.registers[2] = 10;
+    cpu.registers[3] = 10;
+
+    let mem = &mut cpu.memory;
+    mem[0] = 0x80;
+    mem[1] = 0x14;
+    mem[2] = 0x80;
+    mem[3] = 0x24;
+    mem[4] = 0x80;
+    mem[5] = 0x34;
+
+    cpu.run();
+
+    assert_eq!(cpu.registers[0], 35);
 }
