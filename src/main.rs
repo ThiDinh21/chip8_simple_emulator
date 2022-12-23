226806e44fa500b2146064e1f675cb6f6158eb18
Thi Dinh <vietthidinh2001@gmail.com>
2022-12-23 23:04:46 +0200

    add: read opcode from memory

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 struct CPU {
-    current_op: u16,
-    registers: [u8; 2],
+    registers: [u8; 16],
+    program_counter: usize, // position in memory
+    memory: [u8; 0x1000],
 }
 
 impl CPU {
     fn read_opcode(&self) -> u16 {
-        self.current_op
+        let p = self.program_counter;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+        op_byte1 << 8 | op_byte2
     }
 
-    fn run(&self) {
+    fn run(&mut self) {
         // loop {
         let opcode = self.read_opcode();
 
@@ -18,26 +22,31 @@ impl CPU {
         let d = ((opcode & 0x000F) >> 0) as u8;
 
         match (c, x, y, d) {
-            (0x8, _, _, 0x4) => self.add(x, y),
+            (0x8, _, _, 0x4) => self.add_xy(x, y),
             _ => todo!("opcode {opcode:04x}"),
         };
         // }
     }
 
-    fn add(&self, x: u8, y: u8) -> u8 {
-        self.registers[x as usize] + self.registers[y as usize]
+    fn add_xy(&mut self, x: u8, y: u8) {
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+
+        let (val, overflow) = arg1.overflowing_add(arg2);
+        self.registers[x as usize] = val;
+
+        if overflow {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
     }
 }
 
 fn main() {
     let mut cpu = CPU {
-        current_op: 0,
-        registers: [0; 2],
+        registers: [0; 16],
+        program_counter: 0,
+        memory: [0; 0x1000],
     };
-
-    cpu.current_op = 0x8014;
-    cpu.registers[0] = 5;
-    cpu.registers[1] = 10;
-
-    cpu.run();
 }
